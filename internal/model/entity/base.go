586aaa981209ec11aab3c6118a2a230e722fc762
fcy <3181088318@qq.com>
2026-08-18 19:58:32 +0800

    fix:点赞防刷+移除软删除

--- internal/model/entity/base.go
+++ internal/model/entity/base.go
@@ -2,14 +2,11 @@ package entity
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 // BaseEntity 基础实体
 type BaseEntity struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID        uint      `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
